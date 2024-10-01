ed0f69ef827b620c45281358b2104675f722af88
DESKTOP-STEAQ9A\i5 <hoanghung.learn@gmail.com>
2024-10-01 12:37:14 +0700

    + update color

--- server/module/YOLOv8/DetectObject.py
+++ server/module/YOLOv8/DetectObject.py
@@ -1,10 +1,9 @@
 from ultralytics import YOLO
 import cv2
 import os
-from ultralytics import YOLO
-import cv2
-from ultralytics.utils.plotting import Annotator  # ultralytics.yolo.utils.plotting is deprecated
+from ultralytics.utils.plotting import Annotator
 import numpy as np
+import matplotlib.pyplot as plt
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 print(CURRENT_DIR)
@@ -12,22 +11,32 @@ print(CURRENT_DIR)
 class DetectObject:
     def __init__(self):
         modelFile = os.path.join(CURRENT_DIR, "yolov8n.pt")
-        if(os.path.exists(modelFile)):
+        if os.path.exists(modelFile):
             self.model = YOLO(modelFile)
         else:
             raise Exception("Missing model")
-        
+
+    def get_class_colors(self, num_classes):
+        """Generate unique colors for each class using a colormap."""
+        colors = plt.cm.get_cmap('tab20', num_classes)  #
+        return [tuple([int(255 * c) for c in colors(i)[:3]]) for i in range(num_classes)]  # Convert to RGB format
 
     def Detect(self, img):
         results = self.model.predict(img)
-        for r in results:  
+        
+        num_classes = len(self.model.names)
+        class_colors = self.get_class_colors(num_classes)  
+        
+        for r in results:
             annotator = Annotator(img)
             boxes = r.boxes
-            for box in boxes:     
-                b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
-                c = box.cls
-                annotator.box_label(b, self.model.names[int(c)])
-        img = annotator.result()  
+            for box in boxes:
+                b = box.xyxy[0]
+                c = int(box.cls) 
+                class_name = self.model.names[c]
+                color = class_colors[c]  
+                annotator.box_label(b, class_name, color=color)
+        img = annotator.result()
         return img
 
 detectobj = DetectObject()
@@ -35,5 +44,5 @@ detectobj = DetectObject()
 if __name__ == '__main__':
     img = cv2.imread('people.jpg')
     img = detectobj.Detect(img)
-    cv2.imshow('YOLO V8 Detection', img)     
+    cv2.imshow('YOLO V8 Detection', img)
     cv2.waitKey(0)
